dd62c99f676324b0a4981084917b6d8975b2431b
Dy0gu <dbsimoes20@gmail.com>
2024-03-09 02:41:10 +0000

    Add demo command

--- app/cogs/commands.py
+++ app/cogs/commands.py
@@ -1,15 +1,18 @@
 import discord
 from discord.ext import commands
+import logging
+from constants import API_PATH, LOCATIONS
 
 
 class Commands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    # @discord.app_commands.command(name="ping", description="Ping the bot!")
-    # async def ping(self, interaction: discord.Interaction):
-    #     await interaction.response.defer()
-    #     await interaction.followup.send("Pong!")
+    @discord.app_commands.command(name="ping")
+    async def ping(self, interaction: discord.Interaction):
+        logging.debug(f"{interaction.user.name} played ping-pong!")
+        await interaction.response.defer(ephemeral=True)
+        await interaction.followup.send("pong", ephemeral=True)
 
 
 async def setup(bot: commands.Bot):
